dabb1f2513c109ce68a0403f2fd0861ee528413b
SmpManeth <smpmaneth@gmail.com>
2025-05-21 16:37:33 +0530

    chore: delete _form.blade.php, create.blade.php, edit.blade.php and resources/views/pages/customers/index.blade.php

--- resources/views/pages/customers/create.blade.php
+++
@@ -1,8 +0,0 @@
-<x-app-layout>
-<h1 class="text-2xl font-semibold mb-4">Add New Customer</h1>
-
-<form method="POST" action="{{ route('customers.store') }}" class="space-y-6 max-w-xl">
-    @csrf
-    @include('pages.customers._form', ['submit' => 'Create Customer'])
-</form>
-</x-app-layout>
--- resources/views/pages/customers/edit.blade.php
+++
@@ -1,8 +0,0 @@
-<x-app-layout>
-<h1 class="text-2xl font-semibold mb-4">Edit Customer</h1>
-
-<form method="POST" action="{{ route('customers.update', $customer) }}" class="space-y-6 max-w-xl">
-    @csrf @method('PUT')
-    @include('pages.customers._form', ['submit' => 'Update Customer'])
-</form>
-</x-app-layout>
