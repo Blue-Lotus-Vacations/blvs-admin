06e036f6b0b27ec8487c4c946bbd8a89e2454d06
SmpManeth <smpmaneth@gmail.com>
2025-07-22 18:07:16 +0530

    create 5 files and update 3 files

---
+++ resources/views/pages/quotes/create.blade.php
@@ -0,0 +1,8 @@
+<x-app-layout>
+    <div class="p-6">
+        <h2 class="text-xl font-bold mb-4">Add New Quote</h2>
+        <form action="{{ route('quotes.store') }}" method="POST">
+            @include('pages.quotes.form')
+        </form>
+    </div>
+</x-app-layout>
